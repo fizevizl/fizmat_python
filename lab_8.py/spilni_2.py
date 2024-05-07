59dff76b765fc78f74d0afecc195f3642a7e30c1
fizevizl <dnruban@gmail.com>
2024-05-07 19:11:54 +0200

    task 3

--- lab_8.py/spilni_2.py
+++ lab_8.py/spilni_2.py
@@ -2,3 +2,21 @@
 # трикутника, сторонами якого є медіани даного трикутника,
 # створивши об’єкт трикутник
 
+
+class Triangle:
+    def __init__(self, a, b, c) -> None:
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def medians(self):
+        ma = (self.c**2 + self.b**2 - self.a**2) ** 0.5 / 2
+        mb = (self.a**2 + self.c**2 - self.b**2) ** 0.5 / 2
+        mc = (self.a**2 + self.b**2 - self.c**2) ** 0.5 / 2
+        return (ma, mb, mc)
+
+
+tr_big = Triangle(10, 10, 10)
+medians_tr_big = tr_big.medians()
+tr_small = Triangle(*medians_tr_big)
+print(tr_small.medians())         
--- lab_8.py/spilni_3.py
+++ lab_8.py/spilni_3.py
@@ -2,3 +2,33 @@
 # розміром a на b, якщо з неї вирізали чотири круга різного
 # діаметру
 
+import math
+
+class Rectangle:
+    def __init__(self, a, b, r1, r2, r3, r4):
+        self.a = a
+        self.b = b
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
+        self.r4 = r4
+
+    @staticmethod
+    def area_crcl(radius):
+        return math.pi * radius ** 2
+
+    def area(self):
+        return self.a * self.b - self.area_crcl(self.r1) - self.area_crcl(self.r2) - self.area_crcl(self.r3) - self.area_crcl(self.r4)
+    
+    
+rect = Rectangle(100, 100, 20, 20, 10, 10)
+
+print(rect.area())
+
+
+    
+    
+    
+    
+
+
