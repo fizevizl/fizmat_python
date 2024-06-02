fa16021a8ddcd836ce6cb816ac812b0e1f4e2a12
fizevizl <dnruban@gmail.com>
2024-06-02 23:14:47 +0200

    final tasks

--- final_tasks/final_task_3.py
+++ final_tasks/final_task_3.py
@@ -2,3 +2,21 @@
 # При створенні екземпляру класу, екземпляр отримує атрибут sideup зі значенням heads або tails.
 # У класі визначте метод toss, який випадково визначає результат підкидання монети - орел чи решка.
 # Створіть екземпляр класу і виведіть на екран n підкидань монети. Наприклад при n=3 можна отримати heads tails tails
+
+import random
+
+class Coin:
+    def __init__(self):
+        self.sideup = random.choice(["heads","tails"])
+
+    def toss(self):
+        self.sideup = random.choice(["heads","tails"])
+        return self.sideup
+    
+c = Coin()
+n = int(input("введіть число: "))
+
+results = []
+for i in range(n):
+    results.append(c.toss())
+print(results)
--- final_tasks/final_task_4.py
+++ final_tasks/final_task_4.py
@@ -1,25 +1,24 @@
 # У файлі regex_sum_1675329.txt (у прикріплених файлах) знайти кількість чисел та їх суму.
 
-import re
+import re, os
 
 def count_and_sum_numbers(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            text = file.read()
-    except FileNotFoundError:
-        print("Файл не знайдено. Будь ласка, переконайтесь, що шлях до файлу вказаний правильно.")
-        return 0, 0
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    
 
     numbers = re.findall(r'\b\d+\b', text)
-    
     numbers = [int(num) for num in numbers]
     
     count = len(numbers)
     total_sum = sum(numbers)
+    print(numbers)
     
     return count, total_sum
 
-file_path = 'C:/Git/fizmat_python/final_tasks/regex_sum_1675329.txt'  
+path = os.path.dirname(os.path.realpath(__file__))
+file_path = f'{path}/regex_sum_1675329.txt'  
 
 number_count, number_sum = count_and_sum_numbers(file_path)
 
