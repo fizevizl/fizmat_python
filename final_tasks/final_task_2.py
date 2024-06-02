97c1674d8fcffac7e531ccf0063c2a37c789fcaf
fizevizl <dnruban@gmail.com>
2024-06-02 22:35:52 +0200

    final_task4

--- final_tasks/final_task_2.py
+++ final_tasks/final_task_2.py
@@ -1,12 +1,18 @@
 # 2. Напишіть функцію для визначення, чи рік високосний, чи ні.
 
 
+# def is_leap_year(year):
+#     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#         return True
+#     else:
+#         return False
+    
 def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
 
 year = int(input('Введите год: '))
+ans = ['невисокосний', 'високосний']
 
-print(f"{year} рік високосний: {is_leap_year(year)}")
+# print(f"{year} рік високосний: {is_leap_year(year)}")
+print(f"{year} рік {ans[is_leap_year(year)]}")
--- final_tasks/final_task_4.py
+++ final_tasks/final_task_4.py
@@ -1,1 +1,27 @@
 # У файлі regex_sum_1675329.txt (у прикріплених файлах) знайти кількість чисел та їх суму.
+
+import re
+
+def count_and_sum_numbers(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+    except FileNotFoundError:
+        print("Файл не знайдено. Будь ласка, переконайтесь, що шлях до файлу вказаний правильно.")
+        return 0, 0
+
+    numbers = re.findall(r'\b\d+\b', text)
+    
+    numbers = [int(num) for num in numbers]
+    
+    count = len(numbers)
+    total_sum = sum(numbers)
+    
+    return count, total_sum
+
+file_path = 'C:/Git/fizmat_python/final_tasks/regex_sum_1675329.txt'  
+
+number_count, number_sum = count_and_sum_numbers(file_path)
+
+print(f"Кількість чисел у файлі: {number_count}")
+print(f"Сума чисел у файлі: {number_sum}")
